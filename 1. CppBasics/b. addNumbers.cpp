37bff82133aa6c5547e59bff16556542639a2518
nichocd <35509424+nichocd@users.noreply.github.com>
2022-06-03 14:28:19 -0400

    Create b. addNumbers.cpp

---
+++ 1. CppBasics/b. addNumbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c;
+
+    cout<<"Enter two numbers (space): ";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Addition is: "<<c<<endl;
+
+
+    return 0;
+}
