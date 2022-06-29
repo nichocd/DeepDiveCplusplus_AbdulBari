7c3baca207da5053a8f059d9c12f78eec15d3086
nichocd <35509424+nichocd@users.noreply.github.com>
2022-06-29 15:05:09 -0400

    sumNaturalNum

---
+++ 1. CppBasics/e. sumNaturalNum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+using namespace std;
+
+int main(){
+
+    int n, sum;
+    cout<<"Enter number:";
+    cin>>n;
+    sum = n*(n+1)/2;
+    cout<<"Sum is equal to: "<<sum;
+
+    return 0;
+}
