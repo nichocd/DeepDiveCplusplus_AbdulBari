d239a9fc8b41911021a4325b12410491dbd14702
nichocd <35509424+nichocd@users.noreply.github.com>
2022-07-05 14:17:07 -0400

    find max of two numbers using if-else

--- 1. CppBasics/h. maxTwoNumbers.cpp
+++ 1. CppBasics/h. maxTwoNumbers.cpp
@@ -1,2 +1,22 @@
-#inlcude<iostream>
-//
+#include<iostream>
+#include<math.h>
+#include<string>
+using namespace std;
+
+int main(){
+
+    int x,y;
+    cout<<"Enter two numbers(separate w/space):";
+    cin>>x>>y;
+
+    if(x>y){
+        cout<<"Greater number is: "<<x;
+    }
+    else{
+        cout<<"Greater number is: "<<y;
+    }
+
+
+
+    return 0;
+}
