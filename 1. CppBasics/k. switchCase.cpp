c9b285e02119ce84dda58f26a6f4669de6ca0b68
nichocd <35509424+nichocd@users.noreply.github.com>
2022-07-19 15:52:47 -0400

    using switch case for days of week

--- 1. CppBasics/k. switchCase.cpp
+++ 1. CppBasics/k. switchCase.cpp
@@ -6,8 +6,9 @@ using namespace std;
 int main(){
 
     int day;
-    cout<<"Day of Week Number are: Monday[1], Tuesday[2], Wednesday[3], Thursday[4], Friday[5], Saturday[6], Sunday[7]"<<endl;
-    cout<<"Enter day of the week umber: ";
+    cout<<"Day of Week Numbers:\n";
+    cout<<"Monday [1]\nTuesday [2]\nWednesday [3]\nThursday [4]\nFriday [5]\nSaturday [6]\nSunday [7]\n";
+    cout<<"Enter day of the week number: ";
     cin>>day;
 
     switch(day){
