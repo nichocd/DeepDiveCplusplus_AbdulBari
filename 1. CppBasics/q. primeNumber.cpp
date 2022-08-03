910692d4741fd3e7b3a301a8c3998733212fdc35
nichocd <35509424+nichocd@users.noreply.github.com>
2022-08-03 17:01:30 -0400

    find if prime from number given

---
+++ 1. CppBasics/q. primeNumber.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+using namespace std;
+
+int main(){
+
+    int n,i,count=0;
+    cout<<"Enter a number to find prime: ";
+    cin>>n;
+
+    for(i=1;i<n;i++){
+        if(n / i == 0)
+        {
+            count++;
+        }
+
+    }
+    if(count==2)
+        {
+            cout<<n<<" is a prime!"<<endl;
+        }
+        else cout<<n<<" is not a prime!"<<endl;
+
+
+
+    return 0;
+}
