163071a4700f30efb43d4b1fbb71b413088e7d23
nichocd <35509424+nichocd@users.noreply.github.com>
2022-09-12 15:38:10 -0400

    Create e. nestedLoops.cpp

---
+++ 4. Arrays/e. nestedLoops.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+using namespace std;
+
+int main(){
+
+    for(int i=1; i<=5; i++)
+    {
+        for(int j=1; j<=5; j++)
+        {
+            cout<<j<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
