731c216b64d801e3af9323eb53ac241f41fd298e
nichocd <35509424+nichocd@users.noreply.github.com>
2022-09-12 14:50:06 -0400

    Create loopInitial.cpp

---
+++ 3. Loops/loopInitial.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+using namespace std;
+
+int main(){
+
+    int n, i=1;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    //while(i<=n){
+       // cout<<i<<endl;
+       // i++;
+    //}
+    do{
+        cout<<i<<endl;
+        i++;
+    }
+    while(i<=n);
+
+    return 0;
+}
