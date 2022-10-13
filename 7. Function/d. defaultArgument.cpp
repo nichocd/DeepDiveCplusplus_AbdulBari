20e55fa7765599cfa517c5b69568e39c94175813
nichocd <35509424+nichocd@users.noreply.github.com>
2022-10-13 13:41:37 -0400

    default arguments

---
+++ 7. Function/d. defaultArgument.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+#include<string.h>
+using namespace std;
+
+//use one function for overloading with multiple parameters
+
+int add(int x, int y, int z=0)
+{
+    return x+y+z;
+}
+
+int main(){
+
+    cout<<add(5,6)<<endl;
+    cout<<add(5,6,9)<<endl;
+
+
+
+    return 0;
+}
